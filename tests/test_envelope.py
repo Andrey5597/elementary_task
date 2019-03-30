8dccf4955d1a4dd92ba8e58a9b8a12dfabd26705
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-03-30 08:56:13 +0200

    Add 3 tests for class Envelope

---
+++ tests/test_envelope.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest import TestCase
+from elemenrary_task.Task2.envelope import Envelope
+
+
+class TestChessBoard(TestCase):
+
+    def test_constructor(self):
+        envelope = Envelope(7, 8)
+        self.assertEqual((envelope.width, envelope.height), (7, 8))
+
+    def test_less_than(self):
+        envelope1 = Envelope(4, 7)
+        envelope2 = Envelope(5, 18)
+        self.assertLess(envelope1, envelope2)
+
+    def test_greater_than(self):
+        envelope1 = Envelope(20, 20)
+        envelope2 = Envelope(5, 18)
+        self.assertGreater(envelope2, envelope1)
+
+
+if __name__ == '__main__':
+    unittest.main()
